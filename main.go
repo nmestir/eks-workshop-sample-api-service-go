02f586b984a6049cd1af80e22141920af6435a60
nmestir <84922812+nmestir@users.noreply.github.com>
2021-06-01 16:34:00 +0200

    Change Text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Everyone !!!"}
+		res := &response{Message: "Good evening !! "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
